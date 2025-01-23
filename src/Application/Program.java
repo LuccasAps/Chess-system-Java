16d4744cda0c038bb57a2afca51964fdb230de57
Luccas Asaphe <luccasasahe@gmail.com>
2025-01-23 11:25:53 -0300

    Primeira Classe Position

---
+++ src/Application/Program.java
@@ -0,0 +1,11 @@
+package Application;
+
+import BoardGame.Position;
+
+
+public class Program {
+    public static void main(String[] args) {
+        Position position = new Position(5, 8);
+        System.out.print(position);
+    }
+}
